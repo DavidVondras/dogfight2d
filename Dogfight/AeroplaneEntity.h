226a1047dfa96862d2308409a176175f2b0ad489
charles.hetier <charles.hetier@5302af2b-d63e-2d39-01d7-87716617f583>
2010-05-31 16:01:50 +0000

    physic implementation. in progress...

--- Dogfight/AeroplaneEntity.h
+++ Dogfight/AeroplaneEntity.h
@@ -5,6 +5,8 @@
 #include "ObjectEntity.h"
 #include "EventListener.h"
 #include "EnvironementProvider.h"
+#include "CollisionPoint.h"
+#include "AppSettings.h"
 
 ///
 /// Object describing aeroplanes entities
@@ -14,23 +16,27 @@ class AeroplaneEntity :
 {
 private:
 	float _vX, _vY;
+	float _vRotation;
+	float _propelheaderValue;
+	CollisionPoint* _collisionPointArray[ENTITY_MAX_COLLISIONPOINT_NB];
+	int _collisionPointArrayCount;
+
+	sf::Vector2f _Fpoid;
+	sf::Vector2f _FPousee;
+	sf::Vector2f _FRx;
+	sf::Vector2f _FRz;
 
 	sf::Image* _imageAeroplane;
+
 public:
 	//Initialization
 	AeroplaneEntity(void);
 	~AeroplaneEntity(void);
 
-	//Properties
-	float GetVX(void) { return _vX; }
-	void SetVX(float value) { _vX = value; }
-
-	float GetVY(void) { return _vY; }
-	void SetVY(float value) { _vY = value; }
-
 	//Methods
 	virtual void Think(EventListener* eventListener, EnvironementProvider* environementprovider);
 	virtual void Draw(sf::RenderWindow* renderWindow);
+	void DrawStrengthData(sf::RenderWindow* renderWindow);
 };
 
 #endif
